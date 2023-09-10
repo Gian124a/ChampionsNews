86e45878067ecb191bfd61a3e624d998cdbb1d7f
Gian124a <140373193+Gian124a@users.noreply.github.com>
2023-09-10 18:37:45 -0400

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,20 @@
+function Boton(){
+    let Menu = document.getElementById('Menu');
+
+    if (Menu.style.display === 'none') {
+        Menu.style.display = 'initial';
+    }
+    else{
+        Menu.style.display = 'none';
+    }
+}
+function Boton2(){
+    let nav = document.getElementById('nav');
+
+    if (nav.style.display === 'none') {
+        nav.style.display = 'initial';
+    }
+    else{
+        nav.style.display = 'none';
+    }
+}
